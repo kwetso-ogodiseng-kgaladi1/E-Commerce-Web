d2e095d0f2fbcd0165723144a1b5a3febf4c0fd5
kwetso-ogodiseng-kgaladi1 <kgaladikwetso@gmail.com>
2024-04-24 16:58:27 +0200

    correctly imported the main file

--- ecommerce/src/main.js
+++ ecommerce/src/main.js
@@ -1,6 +1,23 @@
-import './assets/main.css'
+/**
+ * main.js
+ *
+ * Bootstraps Vuetify and other plugins then mounts the App`
+ */
 
-import { createApp } from 'vue'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+import './App.vue'
+
+// import { createApp } from 'vue'
+// import App from './App.vue'
+
+// createApp(App).mount('#app')
+
+// Plugins
+import { registerPlugins } from '@/plugins'
+
+const app = createApp(app)
+
+registerPlugins(app)
+
+app.mount('#app')
+
