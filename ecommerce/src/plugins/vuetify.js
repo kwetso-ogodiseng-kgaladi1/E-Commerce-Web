fb72ac7ecb9c344fc32bf04f3a9e6e5192f4bda0
kwetso-ogodiseng-kgaladi1 <kgaladikwetso@gmail.com>
2024-05-13 17:21:30 +0200

    updated

--- ecommerce/src/plugins/vuetify.js
+++ ecommerce/src/plugins/vuetify.js
@@ -5,8 +5,9 @@
  */
 
 // Styles
-import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+
 
 // Composables
 import { createVuetify } from 'vuetify'
